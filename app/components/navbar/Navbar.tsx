5574ea1417c6e7ae01632daa880b9abe17d50ab8
ashwani2 <ashwani.kumar@1406@gmail.com>
2024-08-10 14:20:34 +0530

    add:Modal and button component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
@@ -8,19 +9,20 @@ const NavBar = () => {
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-            <div className="
+          <div
+            className="
             flex
             flex-row
             items-center
             justify-between
             gap-3
             md:gap-0
-            ">
-                <Logo/>
-                <Search/>
-                <UserMenu/>
-
-            </div>
+            "
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
         </Container>
       </div>
     </div>
