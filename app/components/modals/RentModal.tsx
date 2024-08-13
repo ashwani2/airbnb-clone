b9781f08ac3b6681cfad13848c8c57b92b44e399
ashwani2 <ashwani.kumar@1406@gmail.com>
2024-08-13 15:41:44 +0530

    add: 2nd step location of listing

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -42,6 +44,15 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
+  
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -61,13 +72,14 @@ const RentModal = () => {
 
   const actionLabel = useMemo(() => {
     if (step === STEPS.PRICE) {
-      return "create";
+      return "Create";
     }
 
     return "Next";
   }, [step]);
 
   const secondaryActionLabel = useMemo(() => {
+    console.log(step, "Back");
     if (step === STEPS.CATEGORY) {
       return undefined;
     }
@@ -105,13 +117,25 @@ const RentModal = () => {
   );
 
   if (step === STEPS.LOACTION) {
-    bodyContent = <div>LocationStep</div>;
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          onChange={(value) => setCustomValue("location", value)}
+          value={location}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
   }
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
