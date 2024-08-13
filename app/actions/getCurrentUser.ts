f9e8211c445e06190870fa3b8902c87b0738c5e9
ashwani2 <ashwani.kumar@1406@gmail.com>
2024-08-13 15:48:58 +0530

    fix:some authentication error

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/[...nextauth]/authOption";
 import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
